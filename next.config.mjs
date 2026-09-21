952d27b2ab23bf95808610c0eecffcb6670c50b7
khulnagazettenews <khulnagazette.ratul@gmail.com>
2026-09-21 21:39:01 +0600

    Optimize Vercel serverless function storage size

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingExcludes: {
+    '*': [
+      'node_modules/@swc/core-linux-x64-gnu',
+      'node_modules/@swc/core-linux-x64-musl',
+      'node_modules/@esbuild/linux-x64',
+      'node_modules/prisma/build',
+      'node_modules/@prisma/engines',
+      'backup/**/*',
+      'scratch/**/*',
+    ],
+  },
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client', 'better-sqlite3'],
+  },
   images: {
     minimumCacheTTL: 86400,
     remotePatterns: [
@@ -13,4 +27,5 @@ const nextConfig = {
 };
 
 export default nextConfig;
+
 
